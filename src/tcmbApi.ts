f1b6112260f4afd9c1a8555b6a558e7813b0e2ce
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2021-11-18 18:20:46 +0300

    Added typings

--- src/tcmbApi.ts
+++ src/tcmbApi.ts
@@ -1,41 +1,43 @@
 import axios from "axios";
 import xml2js from "xml2js";
-import moment from "moment";
+import moment, {Moment} from "moment";
 
 const tcmbApiUrlToday = 'https://www.tcmb.gov.tr/kurlar/today.xml';
 const tcmbApiUrlTimed = 'https://www.tcmb.gov.tr/kurlar/$year$month/$day$month$year.xml';
 const validCurrencies = ['USD', 'GBP', 'EUR']
 
-const replaceAll = (target, from, to) => target.split(from).join(to)
-const getPreviousWorkday = workday => workday.subtract(workday.day() === 6 ? 1 : (workday.day()  === 0 ? 2 : 0), 'days')
+type Currency = {currency: string, buy: number, sell: number}
+const replaceAll = (target: string, from: string, to: string): string => target.split(from).join(to)
+const getPreviousWorkday = (workday: Moment): Moment =>
+  workday.subtract(workday.day() === 6 ? 1 : (workday.day()  === 0 ? 2 : 0), 'days')
 
-const fetchData = async url =>
+const fetchData = async (url: string): Promise<string> =>
   await axios.get(url)
     .then(res => res.data)
     .catch(() => "")
 
-const generateTcmbUrl = (momentDate) => {
+const generateTcmbUrl = (momentDate: Moment): string => {
   let url = tcmbApiUrlToday
   if(moment().diff(momentDate, 'days') > 0) {
     url = replaceAll(
       replaceAll(
         replaceAll(tcmbApiUrlTimed,
-          '$year', momentDate.year()),
-        '$month', momentDate.month() + 1),
-      '$day', momentDate.date()
+          '$year', momentDate.year().toString()),
+        '$month', (momentDate.month() + 1).toString()),
+      '$day', momentDate.date().toString()
     )
   }
   return url
 }
 
-const parseData = async (rawData, doFilter=true) =>
+const parseData = async (rawData: string, doFilter=true): Promise<Currency[]> =>
   await xml2js.parseStringPromise(rawData)
     .then(result => result.Tarih_Date.Currency)
     .then(currencies => doFilter ? currencies.filter(currency => validCurrencies.includes(currency.$.Kod.toUpperCase())) : currencies)
     .then(currencies => currencies.map(currency => ({currency: currency.$.Kod, buy: currency.ForexBuying[0], sell: currency.ForexSelling[0]})))
     .catch(() => []);
 
-export const getCurrencies = async (momentDate) => {
+export const getCurrencies = async (momentDate: Moment): Promise<Currency[]> => {
   let subs = 0;
   let currencies = [];
   let previousWorkdayMoment;
@@ -49,6 +51,6 @@ export const getCurrencies = async (momentDate) => {
   return currencies
 }
 
-export const getCurrency = async (momentDate, currency) =>
+export const getCurrency = async (momentDate: Moment, currencyCode: string): Promise<Currency> =>
   await getCurrencies(momentDate)
-    .then(currencies => currencies.find(c => c.currency.toUpperCase() === currency.toUpperCase()))
+    .then(currencies => currencies.find(c => c.currency.toUpperCase() === currencyCode.toUpperCase()))
